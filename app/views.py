ce44f63a998bf7d6635faf47ce28c6f0d3037e44
dmitrijmartys <12masek34@mail.ru>
2022-09-16 18:07:12 +0300

    test2

--- app/views.py
+++ app/views.py
@@ -2,10 +2,9 @@ from django.http import HttpResponse
 import pdb
 
 def index(request):
-    x = 0
-
-    some = 'some'
-    a = 'x'
-    s = 1
+    """
+    Тестовая функция.
+    """
+    
     return HttpResponse(f'<h1>{x}<h1>')
  
