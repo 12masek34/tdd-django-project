608a263e13284e8b33b7103866b9cb2f77471237
dmitrijmartys <12masek34@mail.ru>
2022-09-17 10:42:21 +0300

    test5

---
+++ functional_test.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+borwser = webdriver.Firefox(
+    executable_path='/Users/dmitrijmartys/Python/test/core/geckodriver')
+
+browser.get("http://www.python.org")
+print(browser.title)
+asdasdfads
+
