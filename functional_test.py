0c1409a68bb0bf874b55791428c87b0a78104c94
dmitrijmartys <12masek34@mail.ru>
2022-09-17 22:06:40 +0300

    add first test

--- functional_test.py
+++ functional_test.py
@@ -1,11 +1,25 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox(
-    executable_path='/Users/dmitrijmartys/Python/test/core/geckodriver')
 
-browser.get('http://127.0.0.1:8000')
+class NewVisitorTest(unittest.TestCase):
+    """
+    Тест нового постетителя.
+    """
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox(
+           executable_path='/Users/dmitrijmartys/Python/test/core/geckodriver')
 
-browser.close()
+    def tearDown(self):
+        self.browser.close()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self. browser.get('http://127.0.0.1:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Exit')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
 
