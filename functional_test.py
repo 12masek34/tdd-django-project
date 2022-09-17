be980bce862fc1abe4846b35eae8a282ba1ae413
dmitrijmartys <12masek34@mail.ru>
2022-09-17 18:39:21 +0300

    some

--- functional_test.py
+++ functional_test.py
@@ -3,6 +3,9 @@ from selenium import webdriver
 browser = webdriver.Firefox(
     executable_path='/Users/dmitrijmartys/Python/test/core/geckodriver')
 
-browser.get("http://www.python.org")
-print(browser.title)
+browser.get('http://127.0.0.1:8000')
+
+assert 'Django' in browser.title
+
 browser.close()
+
