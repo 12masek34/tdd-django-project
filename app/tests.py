220b4b3ca2c7343275678c91e2097376f54bbb6b
dmitrijmartys <12masek34@mail.ru>
2022-09-18 23:23:54 +0300

    commit

--- app/tests.py
+++ app/tests.py
@@ -10,5 +10,4 @@ class HomePageTest(TestCase):
 
     def test_home_page_return_correct_html(self):
         response = self.client.get('/')
-        self.assertTemplateUsed(response, 'index.html')
-        
+        self.assertTemplateUsed(response, 'index.html')        
