f872eb8dbf20dd87f73bc6454b0bb8f4727f3a3b
dmitrijmartys <12masek34@mail.ru>
2022-09-18 16:55:01 +0300

    add func test

--- app/views.py
+++ app/views.py
@@ -2,6 +2,14 @@ from django.http import HttpResponse
 import pdb
 
 def index(request):
+    
     return HttpResponse('<html><title>To-Do</title></html>')
+
+class a:
+
+    def foo():
+        pass
+
+    
 
 
