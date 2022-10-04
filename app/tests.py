5a484a17d1fbf27d6cbcadcbc91b21fa5d8d6f1a
dmitrijmartys <12masek34@mail.ru>
2022-10-04 22:07:51 +0300

    add test redirect

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from django.test import TestCase
 from app.views import index
 
@@ -7,8 +9,16 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_post_request(self):
         response = self.client.post('/', data={'item_text': 'a new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'a new list item')
+
         self.assertIn('a new list item', response.content.decode())
         self.assertTemplateUsed(response, 'index.html')
+
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertEqual(response['location'], '/')
 
 
 class ItemModelTest(TestCase):
@@ -27,3 +37,7 @@ class ItemModelTest(TestCase):
         second_saved = saved_items[1]
         self.assertEqual(first_saved.text, 'the first (ever) list item')
         self.assertEqual(second_saved.text , 'second item')
+
+    def test_only_save_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,13 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
+from app.models import Item
+
 def index(request):
-    context = {'new_item_text':request.POST.get('item_text', '')}
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+    else:
+        new_item_text = ''
+
+    context = {'new_item_text':new_item_text}
     return render(request, 'index.html', context)
