b4799407588ef5401f6907ec573d03e37b0dcfe9
dmitrijmartys <12masek34@mail.ru>
2022-10-09 12:14:38 +0300

    add

--- app/tests.py
+++ app/tests.py
@@ -62,6 +62,13 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, 'any text2')
 
 
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f'/lists/{correct_list.id}/')
+        self.assertEqual(response.context['list'], correct_list)
+
+
 class NewListTest(TestCase):
 
     def test_can_save_a_POST_request(self):
@@ -75,3 +82,30 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data={'item_text': 'A new item text'})
         list_ = List.objects.first()
         self.assertRedirects(response, f'/lists/{list_.id}/')
+
+
+class NewItemTest(TestCase):
+
+    def test_can_save_a_post_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text': 'A new item for an existing list'},
+        )
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+
+    def test_redirect_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text': 'A new item for an existing list'},
+        )
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, view_list, view_new
+from .views import index, view_list, view_new, add_item
 
 
 urlpatterns = [
     path('', index, name='index'),
     path('lists/new', view_new, name='new_list'),
     path('lists/<int:list_id>/', view_list, name='view_list'),
+    path('lists/<int:list_id>/add_item', add_item, name='add_item'),
 ]
